d039fe3653f3fbfec537a8fde462737384a2b229
BASIC\suyama <daichi.suyama@basic.co.jp>
2020-06-08 11:20:08 +0900

    修正

--- cloudwatchlogToS3/main.py
+++ cloudwatchlogToS3/main.py
@@ -2,9 +2,10 @@ import boto3
 import datetime
 from _datetime import timedelta
 import os
+import time
 
 # ロググループを検索する際のクエリ―
-PREFIX = os.environ.get('TARGET_LOGGROUP')
+TARGET_LOGGROUPS = str(os.environ.get('TARGET_LOGGROUPS')).split(',')
 # ログを保管するs3バケット
 S3_BUCKET = os.environ.get('S3_BUCKET')
 # ログを保管するs3のディレクトリ
@@ -14,54 +15,65 @@ EXPORT_TASK_NAME = "test_task"
 
 
 def lambda_handler(event=None, context=None):
+    responses = []
 
     client = boto3.client('logs')
-    # ロググループ取得
-    log_group = client.describe_log_groups(
-        logGroupNamePrefix=PREFIX)['logGroups'][0]
 
-    # ログストリーム取得
-    log_stoream_response = client.describe_log_streams(
-        logGroupName=log_group['logGroupName'],
-        orderBy='LastEventTime',
-        descending=False,
-        limit=50
-    )
-    log_storeams = log_stoream_response['logStreams']
-    # ロググループ内の全ログストリームを取得する
-    while "nextToken" in log_stoream_response:
+    for taget_loggroup in TARGET_LOGGROUPS:
+        # タスクが終了するまで待つ
+        tasks = client.describe_export_tasks()['exportTasks']
+        while any((task['status']['code'] != 'COMPLETED' for task in tasks)):
+            time.sleep(5)
+            tasks = client.describe_export_tasks()['exportTasks']
+
+        # ロググループ取得
+        log_group = client.describe_log_groups(
+            logGroupNamePrefix=taget_loggroup)['logGroups'][0]
+
+        # ログストリーム取得
         log_stoream_response = client.describe_log_streams(
             logGroupName=log_group['logGroupName'],
             orderBy='LastEventTime',
             descending=False,
-            limit=50,
-            nextToken=log_stoream_response['nextToken']
+            limit=50
         )
-        log_storeams.extend(log_stoream_response['logStreams'])
+        log_storeams = log_stoream_response['logStreams']
+        # ロググループ内の全ログストリームを取得する
+        while "nextToken" in log_stoream_response:
+            log_stoream_response = client.describe_log_streams(
+                logGroupName=log_group['logGroupName'],
+                orderBy='LastEventTime',
+                descending=False,
+                limit=50,
+                nextToken=log_stoream_response['nextToken']
+            )
+            log_storeams.extend(log_stoream_response['logStreams'])
 
-    # 一週間前のログを対象とする
-    now = datetime.datetime.now()
-    end_day = now - timedelta(days=7)
+        # 一週間前のログを対象とする
+        now = datetime.datetime.now()
+        end_day = now - timedelta(days=7)
 
-    # エクスポートタスクを作成
-    response = client.create_export_task(
-        taskName=EXPORT_TASK_NAME,
-        logGroupName=log_group['logGroupName'],
-        fromTime=0,
-        to=int(end_day.timestamp() * 1000),
-        destination=S3_BUCKET, destinationPrefix=S3_DIR
-    )
+        # エクスポートタスクを作成
+        response = client.create_export_task(
+            taskName=EXPORT_TASK_NAME,
+            logGroupName=log_group['logGroupName'],
+            fromTime=0,
+            to=int(end_day.timestamp() * 1000),
+            destination=S3_BUCKET, destinationPrefix=S3_DIR
+        )
 
-    if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-        # 転送したログストリームを削除する
-        for log_stoream in log_storeams:
-            if log_stoream['creationTime'] < int(end_day.timestamp() * 1000):
-                client.delete_log_stream(
-                    logGroupName=log_group['logGroupName'],
-                    logStreamName=log_stoream['logStreamName']
-                )
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            # 転送したログストリームを削除する
+            for log_stoream in log_storeams:
+                if log_stoream['creationTime'] < int(end_day.timestamp() * 1000):
+                    client.delete_log_stream(
+                        logGroupName=log_group['logGroupName'],
+                        logStreamName=log_stoream['logStreamName']
+                    )
 
-    return response
+        responses.append(response)
+
+    return responses
 
 
 lambda_handler()
